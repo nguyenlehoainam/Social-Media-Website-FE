7cbbe0f74e68f1b0b2da005de4249a1b6f2024d2
nguyenlehoainam <namngyenhoai21@gmail.com>
2025-10-08 14:22:22 +0700

    ✨ feat: add forgot-password page

--- Social-Media-Website-FE/src/pages/ForgotPasswordPage/ForgotPasswordPage.jsx
+++ Social-Media-Website-FE/src/pages/ForgotPasswordPage/ForgotPasswordPage.jsx
@@ -3,8 +3,35 @@ import "./ForgotPasswordPage.scss";
 
 function ForgotPasswordPage() {
   return (
-    <div>
-      <h1>hello word</h1>
+    <div className="register-page">
+      <div className="register-page-container">
+        <div className="register-card">
+          <h1 className="register-header">Welcome to Vinbook</h1>
+          <p className="register-text">
+            Confirm your email to continue to sign in
+          </p>
+          <div className="input-group">
+            <div className="email-form">
+              <label className="input-label" htmlFor="email">
+                Email Adress{" "}
+              </label>
+              <input
+                type="email"
+                name="email"
+                required
+                placeholder="nguyenlehoainam@gmail.com"
+              />
+            </div>
+          </div>
+          <button className="register-btn" type="submit">
+            Find Account
+          </button>
+          <div className="footer-text">
+            <a href="/">Login</a>
+            <a href="/register">Register</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
